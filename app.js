2c44bcae61b6220af86ed5f9d79078b6c69823e6
Sakib Hasan <53707185+skb461@users.noreply.github.com>
2024-02-22 22:47:14 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -16,12 +16,22 @@ let ticketprice = 550;
 //initial total ticket price
 let totalAmount=0;
 
+//initial check ticket
+let previous_name=0;
+
+//initial flag
+let flag = 0;
+
 
 //clicking all ticket btn
 ticketBtns.forEach((ticketBtn)=>{
     ticketBtn.addEventListener('click',()=>{
-        
-        ticketBtn.className='btn btn-success py-3 px-5 mx-4 seatPosition'
+
+        if (flag < 4){
+            ticketBtn.className='btn btn-success py-3 px-5 mx-4 seatPosition'
+        } else {
+            ticketBtn.className='btn btn-secondary py-3 px-5 mx-4 seatPosition'
+        }
 
         //getting seat number
         let seatName = ticketBtn.textContent
@@ -34,27 +44,48 @@ ticketBtns.forEach((ticketBtn)=>{
 
         //changing the total amount on table
         totalAmoutDisplay.textContent=totalAmount;
-
-        //changing the grand total
-        grandTotalDisplay.textContent=totalAmount;
     })
 })
 
+//cupone total discount
+function myFunction() {
+    let cuponeTotalDiscount = document.getElementById("coupne").value;
+
+    //changing the grand total
+    if (cuponeTotalDiscount == "NEW15") {
+        grandTotalDisplay.textContent=totalAmount-((totalAmount*15)/100);
+    } else if (cuponeTotalDiscount == "Couple20") {
+        grandTotalDisplay.textContent=totalAmount-((totalAmount*20)/100);
+    } else {
+        grandTotalDisplay.textContent=totalAmount;
+    }
+}
+
 //booking ticket
 function bookingTicket(seatName,price){
+    if (previous_name == seatName) {
+        alert("Wrong Ticket Already Booked!!\nTry Another One.");
+    } else {
+        if (flag < 4){
+            //creating tr
+            let tr = document.createElement('tr')
 
-    //creating tr
-    let tr = document.createElement('tr')
+            //adding table class list
+            tr.className='table bg-transparent'
 
-    //adding table class list
-    tr.className='table bg-transparent'
+            //data in td
+            tr.innerHTML=`<td class="bg-transparent">${seatName}</td>
+            <td class="bg-transparent">Economoy</td>
+            <td class="bg-transparent">${price}</td>`
 
-    //data in td
-    tr.innerHTML=`<td class="bg-transparent">${seatName}</td>
-    <td class="bg-transparent">Economoy</td>
-    <td class="bg-transparent">${price}</td>`
+            //adding tr-data to table
+            ticketInfoTable.appendChild(tr)
 
-    //adding tr-data to table
-    ticketInfoTable.appendChild(tr)
-    
+            previous_name = seatName;
+
+            flag++;
+        } else {
+            alert("You have select more then four seats!!");
+        }
+    }
 }
